9fdbbf7431efa6ee8f806276eb22a46120112875
LitatyHQ <litatyHQ@gmail.com>
2020-10-09 21:31:24 -0400

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,13 @@
 def square_array(array)
-  # your code here
+  numbers = []
+  array.each do |sq_num|
+    numbers << sq_num ** 2
+  end
+  numbers
 end
+
+# numbers = [1,2,3]
+# numbers.each do |sq_num|
+# sq_num * sq_num
+
+# end
